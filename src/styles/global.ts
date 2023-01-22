1181c651a0ffae712632cd19eb5a73c2b84b0378
Patrick Corrêa Muniz <driver.pcmax@gmail.com>
2023-01-22 13:37:05 -0300

    include font default in app

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -318,4 +318,19 @@ export const GlobalStyle = createGlobalStyle`
     [hidden] {
     display: none;
     }
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
+    }
+
+    body {
+        background: ${props => props.theme['gray-300']}
+    }
+
+    body, input, textarea, button {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
 `
