feb8350ae72c8da501216f759ff2ca3b22d83d46
Patrick Corrêa Muniz <driver.pcmax@gmail.com>
2023-01-22 13:32:40 -0300

    inclui reset css e fonte roboto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import './App.css'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
       <div></div>
     </ThemeProvider>
   )
